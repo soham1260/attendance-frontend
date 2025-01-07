ab173d7102ffc4792654e5b41ef402fce840e053
Vedant-Bhukte <vs.bhukte@gmail.com>
2025-01-07 21:14:07 +0530

    Delete attendance

--- src/components/SubjectDetails.js
+++ src/components/SubjectDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Chart from './Chart';
 import Statistics from './Statistics';
@@ -8,9 +8,32 @@ import styles from './SubjectDetails.module.css';
 
 const SubjectDetails = () => {
   // Extract subject from location state
+  const [attendanceExists, setAttendanceExists] = useState(false);
   const location = useLocation();
-  const { subject } = location.state || {};
+  const { subject,  } = location.state || {};
+  const { successMessage } = location.state || {};
   const navigate = useNavigate();
+
+  //console.log(subject);
+
+  useEffect(() => {
+    const checkAttendance = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/attendanceExistCheck?subjectCode=${subject.code}&date=${new Date().toDateString()}`);
+        const result = await response.json();
+
+        if (result.exists) {
+          setAttendanceExists(true);
+        } else {
+          setAttendanceExists(false);
+        }
+      } catch (error) {
+        console.error('Error checking attendance:', error);
+      }
+    };
+
+    checkAttendance();
+  }, [subject.code]);
 
   // Handle case where no subject is passed in location.state
   if (!subject) {
@@ -19,6 +42,11 @@ const SubjectDetails = () => {
 
   return (
     <div className={styles.container}>
+            {successMessage && (
+                <p className={styles['success-message']}>
+                    {successMessage}
+                </p>
+            )}
             <h1>Subject Details</h1>
             <p>Subject Name: {subject.name}</p>
             <p>Subject Code: {subject.code}</p>
@@ -30,19 +58,21 @@ const SubjectDetails = () => {
                 >
                     Home
                 </button>
-                <button
-                    onClick={() =>
+                {!attendanceExists && (
+                    <button
+                      onClick={() =>
                         navigate("/mark-attendance", {
-                            state: {
-                                subjectName: subject.name,
-                                subjectCode: subject.code,
-                                selectedSubject: subject,
-                            },
+                          state: {
+                            subjectName: subject.name,
+                            subjectCode: subject.code,
+                            selectedSubject: subject,
+                          },
                         })
-                    }
-                >
-                    Mark Attendance
-                </button>
+                      }
+                    >
+                      Mark Attendance
+                    </button>
+                )}
                 <button
                     onClick={() =>
                         navigate("/edit-attendance", {
